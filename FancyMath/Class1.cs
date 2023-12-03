bfa59d631d2ecab67a285ff2e04e7de72e2f6ff8
Adriana <57851464+StormingChaos@users.noreply.github.com>
2023-12-03 15:11:04 -0500

    Implemented interfaces for points
    also changed to library

---
+++ FancyMath/Class1.cs
@@ -0,0 +1,44 @@
+﻿namespace FancyMath
+{
+    /// <summary>
+    /// Represents a point in 2D Cartesian coordinates with x and y axes
+    /// </summary>
+    public interface IPoint2D
+    {
+        /// <summary>
+        /// Gets or sets the X coordinate
+        /// </summary>
+        /// <returns><see cref="double"/></returns>
+        double X { get; set; }
+        /// <summary>
+        /// Gets or sets the Y coordinate
+        /// </summary>
+        double Y { get; set; }
+    }
+
+    /// <summary>
+    /// Represents a point in 3D Cartesian coordinates with x, y, and z axes
+    /// </summary>
+    public interface IPoint3D : IPoint2D
+    {
+        /// <summary>
+        /// Gets or sets the Z coordinate
+        /// </summary>
+        double Z { get; set; }
+    }
+
+    class Point2D : IPoint2D
+    {
+        /// <inheritdoc/>
+        public double X { get; set; }
+        /// <inheritdoc/>
+        public double Y { get; set; }
+    }
+
+    class Point3D : IPoint3D
+    {
+        public double Z { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+}
--- Testing/Program.cs
+++ Testing/Program.cs
@@ -1,13 +1,16 @@
 ﻿// maaathhhhhhhhh
 
-namespace FancyMath
+namespace Testing
 {
     static class Program
     {
         internal static void Main(string[] args)
         {
-
+            
         }
     }
+
+    
+
 }
 
